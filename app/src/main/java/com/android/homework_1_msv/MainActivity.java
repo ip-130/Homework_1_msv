4bc877a48e15ce173519081c215b0ad0010e6183
Sergey <ip_130@mail.ru>
2021-06-13 18:24:20 +0300

    Init commit

---
+++ app/src/main/java/com/android/homework_1_msv/MainActivity.java
@@ -0,0 +1,14 @@
+CDpackage com.android.homework_1_msv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
